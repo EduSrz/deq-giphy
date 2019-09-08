e1c5b96abea5f28045870ac06c9b4f0d470daa05
Eduardo Suárez <e.suarez.mx@gmail.com>
2019-09-08 16:40:34 -0500

    modals and shared components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,11 @@ import { FavoritesComponent } from './pages/favorites/favorites.component';
 import { ResultsComponent } from './pages/results/results.component';
 import { GifDetailComponent } from './pages/gif-detail/gif-detail.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { GifPreviewComponent } from './modals/gif-preview/gif-preview.component';
+import { LoginSignupComponent } from './modals/login-signup/login-signup.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { SearchBarComponent } from './shared/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +21,12 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
     FavoritesComponent,
     ResultsComponent,
     GifDetailComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    GifPreviewComponent,
+    LoginSignupComponent,
+    HeaderComponent,
+    FooterComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
