ee95b950a892af4b444a4297a6fa39dcd495c862
Eduardo Suárez <e.suarez.mx@gmail.com>
2019-09-08 21:19:54 -0500

    routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
+// MODULES
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// COMPONENTS
+import { AppComponent } from './app.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
+import { GifDetailComponent } from './pages/gif-detail/gif-detail.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ResultsComponent } from './pages/results/results.component';
+
+const routes: Routes = [
+  {path: '', component: AppComponent, children: [
+    {path: '', component: HomeComponent, pathMatch: 'full'},
+    {path: 'favorites', component: FavoritesComponent},
+    {path: 'search/:id', component: ResultsComponent},
+    {path: 'gif/:id', component: GifDetailComponent}
+  ]},
+  {path: '**', pathMatch: 'full', component: NotFoundComponent}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
