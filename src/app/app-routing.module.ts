21dc402ab724db9a2ed8c3055b990bdbe61b82a5
Eduardo Suárez <e.suarez.mx@gmail.com>
2019-09-10 10:46:04 -0500

    GIF view by added keywords

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,16 @@
 // MODULES
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
 
 // COMPONENTS
-import { AppComponent } from './app.component';
-import { FavoritesComponent } from './pages/favorites/favorites.component';
-import { HomeComponent } from './pages/home/home.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { ResultsComponent } from './pages/results/results.component';
+import { AppComponent } from './app.component'
+import { HomeComponent } from './pages/home/home.component'
+import { NotFoundComponent } from './pages/not-found/not-found.component'
+import { ResultsComponent } from './pages/results/results.component'
 
 const routes: Routes = [
-  {path: '', component: AppComponent, children: [
-    {path: '', component: HomeComponent, pathMatch: 'full'},
-    {path: 'favorites', component: FavoritesComponent},
-    {path: 'search/:id', component: ResultsComponent}
-  ]},
+  {path: '', component: HomeComponent, pathMatch: 'full'},
+  {path: 'search/:keyword', component: ResultsComponent},
   {path: '**', pathMatch: 'full', component: NotFoundComponent}
 ]
 
--- src/app/pages/results/results.component.ts
+++ src/app/pages/results/results.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+import { Component, OnInit } from '@angular/core'
+import { BsModalService, BsModalRef, ModalOptions } from 'ngx-bootstrap/modal'
+import { GifDetailComponent } from '../../modals/gif-detail/gif-detail.component'
+import { NgxMasonryOptions } from 'ngx-masonry'
+import { GiphyService } from '../../services/giphy.service'
 
 @Component({
   selector: 'app-results',
@@ -7,9 +12,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultsComponent implements OnInit {
 
-  constructor() { }
+  public results: Array<any> = []
+  public myOptions: NgxMasonryOptions = {}
+  public loading: boolean = false
+  public showGoUpButton: boolean = false
+  public mainContainer: any
+  public modalReference: BsModalRef
+  public keyword: string = ''
+
+  constructor(private giphyService: GiphyService, private modalService: BsModalService, private route: ActivatedRoute) {}
+
+  showGifDetails(details: any){
+    let options: ModalOptions = {animated: true, class: 'modal-dialog-centered', initialState: details}
+    this.modalReference = this.modalService.show(GifDetailComponent, options);
+  }
+
+  listMatchGifs() {
+    this.mainContainer.style.scrollBehavior = 'smooth'
+    this.mainContainer.style.overflowY = 'hidden'
+    this.loading = true
+    let offset = this.results.length.toString()
+    setTimeout(() => {
+      this.giphyService.searchGifs(offset, this.keyword).subscribe(response => {
+        this.results.push(...response.body.data)
+        this.mainContainer.style.overflowY = 'scroll'
+      }, error => {
+        this.loading = false
+        console.error(error)
+      })
+    }, 750)
+  }
+
+  onMasonryScroll() {
+    this.mainContainer.scrollTop < 400 ? this.showGoUpButton = false : this.showGoUpButton = true
+  }
+
+  scrollTop() {
+    this.mainContainer.scrollTo(0,0)
+  }
 
   ngOnInit() {
+    this.myOptions = {
+      resize: true,
+      fitWidth: true,
+      initLayout: true,
+      horizontalOrder: true,
+      itemSelector: '.masonry-item',
+      transitionDuration: '0.5s'
+    }
+    this.route.params.subscribe(params => {
+      this.keyword = params.keyword
+    })
+    this.mainContainer = document.getElementById('main')
+    this.listMatchGifs()
   }
 
 }
